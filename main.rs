af8f9eb4237ac3f3fa756278a60f85164ea4f380
pramod-korti <pramod.korti@harman.com>
2025-02-13 07:19:56 +0000

    Excersize 2-Task1,2 and 3

--- main.rs
+++ main.rs
@@ -63,6 +63,49 @@ fn get_average_temperature(logr : &mut Vec<f32>) -> (f32)
 }
 //-----E2-Task2--------------------------//
 
+//-----------------------E2-TASK3-MYstring------------------//
+use std::fmt;
+struct MyString {
+    data: Vec<u8>,
+}
+
+impl MyString {
+    // Create a new MyString
+    fn new(s: &str) -> Self {
+        MyString {
+            data: s.as_bytes().to_vec(),
+        }
+    }
+
+    // Append a string slice to the MyString
+    fn push_str(&mut self, s: &str) {
+        self.data.extend_from_slice(s.as_bytes());
+
+    }
+
+    // Get the length of the MyString
+    fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    // Get the string as a &str
+    fn as_str(&self) -> &str {
+        // Converting the byte vector back into a &str slice
+        std::str::from_utf8(&self.data).unwrap()
+    }
+
+}
+
+// Implementing the fmt::Display trait to allow pretty printing
+impl fmt::Display for MyString {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
+
+
+//-----------------------E2-TASK3-End----------------------//
+
 //---------------------EXCERISE-2----------------------------------//
 fn main()
 {
@@ -89,7 +132,6 @@ fn main()
 
 let mut logger: Vec<f32> = vec![];
 
-//-----E2-Task2-------------------------//
 log_reading(&mut logger,2.0);
 log_reading(&mut logger,3.0);
 log_reading(&mut logger,5.0);
@@ -102,5 +144,21 @@ log_reading(&mut logger,9.0);
 
 let avg2 = get_average_temperature(&mut logger);
 println!("average-2= {}", avg2);
+//-----E2-Task2-------------------------//
+
+//-----E2-Task3-------------------------//
+
+
+
+//-----E2-Task3-------------------------//
+let mut my_string = MyString::new("Welcome");
+
+println!("MyString: {}", my_string);
+
+my_string.push_str(", Home!");
+println!("MyString after push_str: {}", my_string);
+
+println!("Length: {}", my_string.len());
+
 
 }
